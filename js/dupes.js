722522e888ee21b758d288b578160879bc7240b2
Imaparadox <stevemarsh117@gmail.com>
2021-08-04 15:28:47 -0400

    Testing ground complete

---
+++ js/dupes.js
@@ -0,0 +1,17 @@
+const numbers = [41, 24, 28, 1, 40, 41, 32, 33, 50, 5, 34, 5, 21, 21, 43, 43, 21, 4, 49, 24];
+const mostDuplicates = (arr) => {
+
+};
+
+console.log(mostDuplicates(numbers));
+//21 appeared 3 times
+//create variable to track value appearing most
+//create variable to track how many times it is appearing
+//loop over array (i)
+//Start a counter for number of occurrences
+//Loop over array again (j)
+//compare indexes
+//if same, increase counter
+//if counter > current most appearing count
+//update "most" variables to match
+//return most appearing value and count 
